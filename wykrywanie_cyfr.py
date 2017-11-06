1f8c70d6d66adf583f244f5e50e0acc13062e727
Mateusz Urbaniak <31575249+UrbaniakM@users.noreply.github.com>
2017-11-06 10:52:00 +0100

    Finished dividing image into segments with edges

--- wykrywanie_cyfr.py
+++ wykrywanie_cyfr.py
@@ -5,47 +5,42 @@ Kajetan Zimniak'''
 import matplotlib.pyplot as plt
 import numpy as np
 from skimage import data, io, measure, color, feature, filters, exposure, img_as_ubyte, morphology, transform
+from math import ceil
 
 def polygon_area(x,y):
     return 0.5*np.abs(np.dot(x,np.roll(y,1))-np.dot(y,np.roll(x,1)))
 
-def recognition_sign(coords1,coords0):
-    #plt.plot(coords1,coords0)
-    #plt.show()
-    #todo
-    print("")
+def recognize_if_sign(adjusted_image, k):
+    subPlt = plt.subplot(5,2,k+1)
+    plt.axis('off')
+    subPlt.set_aspect('equal')
+    io.imshow(adjusted_image)
     
 def process_image(image_input, k):
-    #subPlt = plt.subplot(25,4,k+1)
-    subPlt = plt.subplot(1,2,k+1)
-    plt.axis('off')
+    
+    #subPlt = plt.subplot(1,2,k+1)
+    #
     image = color.rgb2gray(image_input)
     percentileP, percentileK = np.percentile(image,(2,98))
     image = exposure.rescale_intensity(image,in_range=(percentileP,percentileK))
+    #TODO - noise removing
     edges = measure.find_contours(image, level=0.3, fully_connected='low', positive_orientation='high')
-    subPlt.set_aspect('equal')
-    minCoord = [] #potrzebne do wycinania krawedzi
-    maxCoord = [] #potrzebne do wycinania krawedzi
-    temp = True #potrzebne do wycinania krawedzi
+    array_min_max = []
     for n, coords in enumerate(edges):
         sizeOfPolygon = polygon_area(coords[:,1],coords[:,0])
         if (sizeOfPolygon > 50) and (coords[0,1] == coords[-1,1]) and (coords[0,0] == coords[-1,0]):
-            print(np.min(coords[:,1]))
-            print(np.max(coords[:,1]))
-
-            plt.plot(coords[:, 1], coords[:, 0], linewidth=1)
-            plt.plot(np.mean(coords[:,1]),np.mean(coords[:,0]), 'wo', markersize=3)
-        #Sprawdzanie czy to nie jest czesc juz gotowej cyfry
-        minCoord.append(min(coords[:,1]))
-        maxCoord.append(max(coords[:,1]))
-        for a,minValue in enumerate(minCoord):
-            if maxCoord[a] > min(coords[:,1]) and min(coords[:,1]) > minCoord[a]:
-                temp = False 
-        if temp == True:
-            recognition_sign(coords[:,1],coords[:,0])
-        
-    subPlt.set_aspect('equal')
-    io.imshow(image)
+            xMin, xMax = (np.min(coords[:,1]),np.max(coords[:,1]))
+            yMin, yMax = (np.min(coords[:,0]),np.max(coords[:,0]))
+            not_inside = True
+            for instance in array_min_max:
+               if (xMin > instance[0]) and (xMax < instance[1]) and (yMin > instance[2]) and (yMax < instance[3]):
+                   not_inside = False
+            if not_inside == True:
+                array_min_max.append([int(xMin), ceil(xMax), int(yMin), ceil(yMax)]) #min - round down, max - round up
+    for n,instance in enumerate(array_min_max):
+        adjusted_image = image[instance[2]:instance[3],instance[0]:instance[1]] #cropping - image[ymin:ymax,xmin:xmax]
+        # RESIZE? RESCALE? etc.
+        recognize_if_sign(adjusted_image, n)
 
 if __name__ == '__main__':
     plt.figure(figsize=(50,100))

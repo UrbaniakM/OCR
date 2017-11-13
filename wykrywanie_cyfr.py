c36c115e436b8020e846065c93154b1da60d70e2
Mateusz Urbaniak <31575249+UrbaniakM@users.noreply.github.com>
2017-11-13 20:37:41 +0100

    Prostokat dookola wykrytych cyfr

--- wykrywanie_cyfr.py
+++ wykrywanie_cyfr.py
@@ -1,21 +1,31 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from skimage import data, io, measure, color, feature, filters, exposure, img_as_ubyte, morphology, transform
+from skimage import data, io, measure, color, feature, filters, exposure, img_as_ubyte, morphology, transform, draw
 from math import ceil
 from skimage.feature import match_template
 
 def polygon_area(x,y):
     return 0.5*np.abs(np.dot(x,np.roll(y,1))-np.dot(y,np.roll(x,1)))
 
-def recognize_if_sign(adjusted_image, k):
-    subPlt = plt.subplot(50,2,k+1)
-    plt.axis('off')
-    subPlt.set_aspect('equal')
-    io.imshow(adjusted_image)
+def recognize_if_sign(adjusted_image, k, templates):
+    result = -1
+    for x in range(10):        
+            if match_template(adjusted_image, templates[x]) > 0.7:
+                result = x
+                break
+    return result
+
+def draw_rectangle(image,x_min, y_min, x_max, y_max):
+    rr,cc = draw.polygon_perimeter([x_min, x_max, x_max, x_min], [y_min, y_min, y_max, y_max], shape=None, clip=False)
+    image[rr,cc] = (0,255,255)
+    return image
+
     
 def process_image(image_input, k, templates_array):
-    
-    #subPlt = plt.subplot(1,2,k+1)
+    subPlt = plt.subplot(3,1,k+1)
+    plt.axis('off')
+    subPlt.set_aspect('equal')
+
     image = color.rgb2gray(image_input)
     percentileP, percentileK = np.percentile(image,(2,98))
     image = exposure.rescale_intensity(image,in_range=(percentileP,percentileK))
@@ -35,16 +45,12 @@ def process_image(image_input, k, templates_array):
                 array_min_max.append([int(xMin), ceil(xMax), int(yMin), ceil(yMax)]) #min - round down, max - round up
     for n,instance in enumerate(array_min_max):
         adjusted_image = image[instance[2]:instance[3],instance[0]:instance[1]] #cropping - image[ymin:ymax,xmin:xmax]
-        # RESIZE? RESCALE? etc.
-        #result = match_template(adjusted_image, template1)
-        adjusted_image = transform.resize(adjusted_image, (30, 30),mode='reflect')
-        for x in range(10):        
-            if match_template(adjusted_image, templates_array[x]) > 0.7:
-                print(x)
-                break
-        #print(match_template(adjusted_image, templates_array[x]))            
-        
-        recognize_if_sign(adjusted_image, n)
+        adjusted_image = transform.resize(adjusted_image, (24, 16),mode='reflect')         
+        res = recognize_if_sign(adjusted_image, n, templates_array)
+        if res != -1:
+           print(res)
+           image_input = draw_rectangle(image_input,instance[2],instance[0],instance[3],instance[1])
+    io.imshow(image_input)
 
 if __name__ == '__main__':
     plt.figure(figsize=(50,100))
@@ -52,7 +58,7 @@ if __name__ == '__main__':
     templates_input = io.ImageCollection('data_sets/*.jpg')
     templates = []
     for template in templates_input:
-       	template = transform.resize(template, (30, 30),mode='reflect')
+       	template = transform.resize(template, (24, 16),mode='reflect')
         templates.append(color.rgb2gray(template))
     for n,image in enumerate(images):
         process_image(image,n, templates)
